7f67792e94d93cc66211bb2b8b213ad0ce0c0af0
Augusto Lyrio <augustoplyrio@gmail.com>
2018-10-12 11:28:56 -0300

    Crazzy Puzzle

---
+++ Scripts/Portal.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour {
+
+    private Vector3 Eu;
+
+	// Use this for initialization
+	void Start () {
+
+        Eu = transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+
+	}
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.tag == "Tiradente")
+        {
+            transform.position = new Vector3(-33, 10, -1);
+        }
+
+        if (coll.gameObject.tag == "Bola")
+        {
+            transform.position = Eu;
+        }
+
+        if (coll.gameObject.tag == "Paqueta")
+        {
+            transform.position = Eu;
+        }
+
+    }
+
+
+}
